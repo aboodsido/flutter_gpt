824891afcefe129c3c894011d4e964e81586bab4
Abdullah Abo-Sido <100968332+aboodsido@users.noreply.github.com>
2023-10-04 19:31:32 +0300

    Update app_consts.dart

--- lib/constants/app_consts.dart
+++ lib/constants/app_consts.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 class AppConsts {
-  static String apiKey = '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';
+  static String apiKey = 'sk-Pxa***************************************L7';
   static Color kPrimaryColor = const Color.fromRGBO(0, 188, 212, 100);
 }
